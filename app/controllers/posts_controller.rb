bd76286f649bd18e407c0195e933f9e533ea6f0e
moru0606 <moru150606@gmail.com>
2021-11-06 23:11:25 +0900

    feat: post投稿機能追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = '投稿されました'
+      redirect_to @post
+    else   
+      flash.now[:danger] = '投稿できませんでした'
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:success] = '投稿は削除されました'
+    redirect_to posts_url
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :title)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { minimum: 10 }
 end

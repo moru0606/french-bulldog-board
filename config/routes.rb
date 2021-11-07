db5810524c854a8fc1be8bd98847d8a8df9f2230
moru0606 <moru150606@gmail.com>
2021-11-07 23:29:51 +0900

    feat: ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   root to: "posts#index"
 
   get 'signup', to: 'users#new'
